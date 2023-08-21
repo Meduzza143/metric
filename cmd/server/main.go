bcc8741874dbda661b9e5dc7f198b73da214c9e3
Meduzza <ice143@yandex.ru>
2023-08-21 18:29:12 +0300

    increment 1

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,3 +1,19 @@
 package main
 
-func main() {}
+import (
+	"net/http"
+
+	handlers "github.com/Meduzza143/metric/cmd/server/internal"
+)
+
+func main() {
+
+	mux := http.NewServeMux()
+	//http://<АДРЕС_СЕРВЕРА>/update/<ТИП_МЕТРИКИ>/<ИМЯ_МЕТРИКИ>/<ЗНАЧЕНИЕ_МЕТРИКИ>
+	mux.HandleFunc(`/update/`, handlers.UpdateHandle)
+
+	err := http.ListenAndServe(`localhost:8080`, mux)
+	if err != nil {
+		panic(err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Meduzza143/metric
+
+go 1.20
